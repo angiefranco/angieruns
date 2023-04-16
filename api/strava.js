df6995629d160042c53aac3b4924d24c2d847d3f
Angie Franco <afranco4@nd.edu>
2023-04-15 23:36:53 -0400

    progress bar

--- api/strava.js
+++ api/strava.js
@@ -3,4 +3,8 @@ export default async (req,res)=>{
         'Accept': 'application/json, text/plain, */*',
         'Content-Type': 'application/json'
     }
+
+    const body = JSON.stringify({
+        client_id: process.env.STRAVA_CLIENT_ID
+    })
 }
